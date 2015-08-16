f81b0b3ba6a4e874c8b9e935b5cd401279ba1314
ATILAY <atiyil@gmail.com>
2015-08-16 10:06:45 +0300

    Update MissingInteger.rb
    %75 correctness

--- Codility/MissingInteger.rb
+++ Codility/MissingInteger.rb
@@ -1,12 +1,21 @@
 def solution(a)
     a.uniq!
     a.sort!
-    #print a
     i = 0
+    counter = 1
     while i < a.count
-        if a[i] != i + 1
-            return i + 1
+        if a[i] < 1 
+            i += 1
+            next
+        elsif a[i] > counter
+            return counter
         end
         i += 1
+        counter += 1
+    end
+    if a.last > 0 and i = a.count
+        return i + 1
+    else 
+        return 1
     end
 end
