541e770b9f2426e53d892b7d2cb62f843ad0f49a
ATILAY <atiyil@gmail.com>
2015-08-20 20:45:08 +0300

    Update MaxCounters.rb

--- Codility/MaxCounters.rb
+++ Codility/MaxCounters.rb
@@ -2,14 +2,20 @@ def solution(n, a)
   counters = Array.new(n,0)
   #print counters
   maxi = 0
+  againMaxCounters = false
   i = 0
   while i < a.count
-    if a[i] > n
+    if !againMaxCounters and a[i] > n
       counters.fill(maxi)
-    else
+      againMaxCounters = true
+    elsif a[i] <= n
       counters[a[i]-1] += 1
+      againMaxCounters = false
+      if maxi < counters[a[i]-1]
+          maxi = counters[a[i]-1]
+      end
     end
-    maxi = counters.max
+    #print counters
     i+=1
   end
   #print counters
