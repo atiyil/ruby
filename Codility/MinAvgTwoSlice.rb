a1ad8f7aa031edaab4e1e38d98d8bed10f2d5879
ATILAY <atiyil@gmail.com>
2015-09-06 22:48:58 +0300

    Update MinAvgTwoSlice.rb

--- Codility/MinAvgTwoSlice.rb
+++ Codility/MinAvgTwoSlice.rb
@@ -1,24 +1,32 @@
-
 def solution(a)
-  i = 0
-  min = 10000
-  minSum = 20000
+  i = 2
   index = 0
-  minSlice = 1000000000
-  successiveSum = 0
+  sliceSum = a[0].to_i + a[1].to_i
+  sliceAvg = sliceSum.to_f / 2
+  sliceLen = 2
+  #average = a.inject(:+).to_f / a.count #average of the array
   while i < a.count 
-  #find minimum value in the array
-    if a[i] < min
-      min = a[i]
-    end
-    #find minimum sum in the array
-    if i+1 < a.count and a[i].to_i + a[i+1].to_i < minSum
-      minSum = a[i].to_i + a[i+1].to_i
-      index = i
+    #find minimum average
+    if index + sliceLen >= i 
+      if (a[i].to_i + sliceSum).to_f / (sliceLen + 1) < sliceAvg
+        sliceSum += a[i].to_i 
+        sliceLen += 1
+        sliceAvg = sliceSum.to_f / sliceLen
+      end
+      if a[i] < a[i - 2] and (a[i].to_i + a[i-1].to_i).to_f / 2 < sliceAvg
+        sliceSum = a[i].to_i + a[i-1].to_i
+        sliceLen = 2
+        sliceAvg = sliceSum.to_f / sliceLen
+        index = i - 1
+      end
+    elsif (a[i].to_i + a[i-1].to_i).to_f / 2 < sliceAvg
+        sliceSum = a[i].to_i + a[i-1].to_i
+        sliceLen = 2
+        sliceAvg = sliceSum.to_f / sliceLen
+        index = i - 1
     end
     i += 1
   end
-  puts min, minSum
+  #puts sliceSum, sliceAvg, average
   return index
-  
 end
